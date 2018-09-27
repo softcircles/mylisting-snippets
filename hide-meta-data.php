67cecd43a3b4e20f05f5375ad755793691dae8be
softcircles <softcircles@outlook.com>
2018-09-27 13:27:34 +0500

    Update hide-meta-data.php

--- hide-meta-data.php
+++ hide-meta-data.php
@@ -1,4 +1,8 @@
-// Use the following snippet to hide the description while sharing the post on Facebook.
+/**
+ * SNIPPET #1 - Hide description field
+ *
+ * Use the following snippet to hide the description while sharing the post on Facebook.
+ */
 
 add_filter( 'mylisting\single\og:tags', function( $tags ) {
 
@@ -7,4 +11,14 @@ add_filter( 'mylisting\single\og:tags', function( $tags ) {
     }
 
     return $tags;
+} );
+
+/**
+ * SNIPPET #2 - Display the cover image instead of logo
+ * 
+ * Use the following snippet to display the cover image instead of the logo while sharing on facebook. 
+ */
+
+add_filter( 'mylisting\single\og:image', function() {
+    return 'cover';
 } );
