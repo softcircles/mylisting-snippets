cc8caca18ec49f6728efd165a8eeecf2f69d309a
softcircles <softcircles@outlook.com>
2018-10-01 15:19:56 +0500

    Create gravity-form-listing-fix.php
    Use the following snippet to fix the gravity form error on listing preview

---
+++ gravity-form-listing-fix.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *
+ * Use the following snippet to fix the gravity form error on listing preview
+ *
+ */
+
+if ( isset( $_REQUEST['job_manager_form'] )  && $_REQUEST['job_manager_form'] == 'submit-job' ) {
+
+    $unset_gform_fields = [
+        'gform_ajax',
+        'gform_submit',
+        'gform_unique_id',
+        'gform_target_page_number_2',
+        'gform_source_page_number_2',
+        'gform_field_values'
+    ];
+
+    foreach ( $unset_gform_fields as $field) {
+        if ( ! isset( $_POST[ $field ] ) ) {
+            continue;
+        }
+
+        unset( $_POST[ $field ] );
+    }
+}
