f5ca32c286ddda2608332b5510841bb59d5b5f0e
softcircles <softcircles@outlook.com>
2018-12-14 17:53:08 +0500

    Update confirm-logout.php

--- confirm-logout.php
+++ confirm-logout.php
@@ -4,11 +4,22 @@
 * Skip Logout Message
 */
 
+add_action('init', function () {
+    if ( ! isset( $_GET['action'] ) || 'logout' != $_GET['action'] || 
+        'wp-login.php' != $GLOBALS['pagenow'] ) {
+        return null;
+    }
+    
+    wp_safe_redirect( site_url('/') );
+    exit;
+});
+
 function wc_bypass_logout_confirmation() {
     global $wp;
   
     if ( isset( $wp->query_vars['customer-logout'] ) ) {
-        wp_redirect( str_replace( '&amp;', '&', wp_logout_url( wc_get_page_permalink( 'myaccount' ) ) ) );
+        wp_logout();
+        wp_safe_redirect( str_replace( ['&#038;', '&amp;'], '&', wp_logout_url( wc_get_page_permalink( 'myaccount' ) ) ) );
         exit;
     }
 }
