916cb4238e271bdde67fe5e149dd56ab43208f70
softcircles <softcircles@outlook.com>
2018-12-21 17:31:01 +0500

    Hide-Past-Events.php

---
+++ Hide-Past-Events.php
@@ -0,0 +1,40 @@
+add_action( 'mylisting/get-listings/before-query', function ( &$args ) {
+    $listing_types = ['event'];
+    
+    if ( empty( $args['meta_query'] ) || empty( $args['meta_query']['listing_type_query'] ) ) {
+        return $args;
+    }
+    
+    $meta_value = $args['meta_query']['listing_type_query']['value'];
+    
+    if ( ! isset( $meta_value ) ) {
+        return $args;
+    }
+    
+    if ( is_array( $meta_value ) && ! empty( $meta_value ) ) {
+        $meta_value = $meta_value[0];
+    }
+    
+    // Only apply to the listing types defined in $listing_types.
+    if ( ! in_array( $meta_value, $listing_types ) ) {
+        return $args;
+    }
+
+    $args['meta_query'][] = [
+        'relation' => 'OR',
+        [
+            'key' => '_job_date',
+            'value' => date('Y-m-d'),
+            'compare' => '>=',
+            'type' => 'DATE',
+        ],
+        [
+            'key' => '_job_date',
+            'value' => '',
+        ],
+        [
+            'key' => '_job_date',
+            'compare' => 'NOT EXISTS',
+        ],
+    ];
+}, 10 );
