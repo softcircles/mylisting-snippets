6d1b95a43d8eae2fb2b42335ea169b90833fe5e5
softcircles <softcircles@outlook.com>
2019-01-31 13:13:25 +0500

    Create change-user-role-on-submit-listing.php

---
+++ change-user-role-on-submit-listing.php
@@ -0,0 +1,27 @@
+<?php
+
+function update_roles()
+{
+ 
+   global $wpdb;
+ 
+   // Get the author
+   $author = wp_get_current_user();
+   // Get post by author
+   $posts = $wpdb->get_results("SELECT * FROM $wpdb->posts WHERE post_author = " . $author->ID );
+ 
+   $numPost = count($posts);
+ 
+   // Do the checks to see if they have the roles and if not update them.
+   if($numPost > 0 && current_user_can('subscriber'))
+   {
+       // Add role
+       $author->add_role( 'editor' );
+        
+       // Remove role
+       $author->remove_role( 'subscriber' );
+ 
+   }
+}
+ 
+add_action('publish_job_listing', 'update_roles');
