86969b2bc073d2db207f04084a86ae9c50f450ce
softcircles <softcircles@outlook.com>
2019-02-12 15:10:23 +0500

    Update filename-of-contact-form-attachments.php

--- filename-of-contact-form-attachments.php
+++ filename-of-contact-form-attachments.php
@@ -1,18 +1,18 @@
 <?php
 
 add_filter( 'wpcf7_upload_file_name', function( $filename, $file_name, $tag ) {
-
+    
     $name = $tag->name;
     $id = $tag->get_id_option();
-
+    
     $file = isset( $_FILES[$name] ) ? $_FILES[$name] : null;
-
     if ( isset( $_POST['_case27_post_id'] ) && ! empty( $_POST['_case27_post_id'] ) && $file ) {
         $path = pathinfo( $file[ 'name' ] );
-        $file[ 'name' ] = sanitize_title( get_the_title( $_POST['_case27_post_id'] ) ) . '.' . $path[ 'extension' ];
-        $filename = $file['name'];
+        
+        $name = explode( '.', $file[ 'name' ] );
+        
+        $filename = 'twwork' . $name[0] . '.' . $path[ 'extension' ];
+
     }
-
     return $filename;
-
 }, 99, 3 );
