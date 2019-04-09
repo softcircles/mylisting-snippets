7ac9af479ad73fbf28fc71bc9bbd9bfa1b033980
softcircles <softcircles@outlook.com>
2019-04-09 14:55:32 +0500

    Update woocommerce-redirect-after-login-and-register.php

--- woocommerce-redirect-after-login-and-register.php
+++ woocommerce-redirect-after-login-and-register.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Redirect to shop after login.
+ * Redirect to home after login.
  *
  * @param $redirect
  * @param $user
@@ -9,21 +9,21 @@
  * @return false|string
  */
 function iconic_login_redirect( $redirect, $user ) {
-    return wc_get_page_permalink( 'shop' );
+    return home_url( '/' );
 }
 
 add_filter( 'woocommerce_login_redirect', 'iconic_login_redirect' );
 
 
 /**
- * Redirect after registration.
+ * Redirect to home after registration.
  *
  * @param $redirect
  *
  * @return string
  */
 function iconic_register_redirect( $redirect ) {
-	return wc_get_page_permalink( 'shop' );
+    return home_url( '/' );
 }
 
 add_filter( 'woocommerce_registration_redirect', 'iconic_register_redirect' );
