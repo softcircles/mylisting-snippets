e595ea70922504eb80229e9ba02e754bb20329ad
softcircles <softcircles@outlook.com>
2019-05-21 00:55:21 +0500

    Create custom-field-admin-listing.php

---
+++ custom-field-admin-listing.php
@@ -0,0 +1,39 @@
+<?php
+
+add_filter( 'manage_edit-job_listing_columns', function( $defaults ) {
+    $defaults['custom_field'] = esc_html__( 'Custom Field', 'my-listing' );
+    return $defaults;
+});
+
+add_filter( 'manage_job_listing_posts_custom_column', function( $column ) {
+    global $post;
+
+    if ( ! ( $listing = \MyListing\Src\Listing::get( $post ) ) ) {
+        return;
+    }
+
+    $_value = absint( get_post_meta( $listing->get_id(), '_custom_field', true ) );
+
+    if ( $column === 'custom_field' && $_value ) {
+        $url = add_query_arg( [
+            'post_type'                 => 'job_listing',
+            'filter_by_custom_field'    => $_value,
+        ], admin_url( 'edit.php' ) );
+        printf( '<a href="%s">%s</a>', esc_url( $url ), $_value );
+    }
+
+    return $column;
+});
+add_filter( 'parse_query', function( $query ) {
+
+    global $typenow;
+
+    if ( $typenow !== 'job_listing' || empty( $_GET['filter_by_custom_field'] ) || ! is_admin() ) {
+        return $query;
+    }
+
+    $query->query_vars['meta_key']   = '_custom_field';
+    $query->query_vars['meta_value'] = $_GET['filter_by_custom_field'];
+
+    return $query;
+});
