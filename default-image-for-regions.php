64d2493a6a0973dc294250d1f7efb3d71b57fda2
softcircles <softcircles@outlook.com>
2019-09-02 13:22:48 +0500

    Create default-image-for-regions.php

---
+++ default-image-for-regions.php
@@ -0,0 +1,22 @@
+<?php
+
+add_action( 'init', function() {
+		$terms = get_terms( [
+			'taxonomy'		=> 'region',
+			'hide_empty'	=> false,
+		] );
+
+		if ( is_wp_error( $terms ) ) {
+			return;
+		}
+
+		foreach ( $terms as $term ) {
+			$term = new MyListing\Src\Term( $term );
+			$image = $term->get_image();
+
+			if ( ! $image ) {
+				update_term_meta( $term->get_id(), 'image', 3689 );
+				//printf( get_term_meta( $term->get_id() ) );
+			}
+		}
+} );
