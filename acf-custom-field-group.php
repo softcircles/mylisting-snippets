a0f68789a36137f3945af79d209ef149666b3d44
softcircles <softcircles@outlook.com>
2019-09-03 14:03:49 +0500

    Create acf-custom-field-group.php

---
+++ acf-custom-field-group.php
@@ -0,0 +1,66 @@
+<?php
+
+add_action('acf/init', function() {
+    
+    acf_add_local_field_group(array(
+        'key' => 'group_5d6cccff71133',
+        'title' => 'SEO',
+        'fields' => array(
+            array(
+                'key' => 'field_5d6ccd186e982',
+                'label' => 'SEO keywords',
+                'name' => 'seo_keywords',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5d6ccd733e2a1',
+                'label' => 'SEO description',
+                'name' => 'seo_description',
+                'type' => 'textarea',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'maxlength' => '',
+                'rows' => '',
+                'new_lines' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'taxonomy',
+                    'operator' => '==',
+                    'value' => 'job_listing_category',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => 1,
+        'description' => '',
+    ));
+});
