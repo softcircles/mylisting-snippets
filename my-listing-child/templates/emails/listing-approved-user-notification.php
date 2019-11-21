d1ff11957a381910aee71f0d0eb27098194c6725
softcircles <softcircles@outlook.com>
2019-11-21 16:07:31 +0500

    Create listing-approved-user-notification.php

---
+++ my-listing-child/templates/emails/listing-approved-user-notification.php
@@ -0,0 +1,5 @@
+<?php require locate_template( 'templates/emails/partials/header.php' ) ?>
+ 
+*Email body contents go here*
+ 
+<?php require locate_template( 'templates/emails/partials/footer.php' ) ?>
