539cf5d16bbc594b8198654662ddb27c19b40965
softcircles <softcircles@outlook.com>
2020-03-16 13:18:11 +0500

    Update table-block.php

--- templates/single-listing/content-blocks/table-block.php
+++ templates/single-listing/content-blocks/table-block.php
@@ -17,21 +17,30 @@ if ( empty( $rows ) ) {
 <div class="<?php echo esc_attr( $block->get_wrapper_classes() ) ?>" id="<?php echo esc_attr( $block->get_wrapper_id() ) ?>">
     <div class="element table-block">
         <div class="pf-head">
-			<div class="title-style-1">
-				<i class="<?php echo esc_attr( $block->get_icon() ) ?>"></i>
-				<h5><?php echo esc_html( $block->get_title() ) ?></h5>
-			</div>
+            <div class="title-style-1">
+                <i class="<?php echo esc_attr( $block->get_icon() ) ?>"></i>
+                <h5><?php echo esc_html( $block->get_title() ) ?></h5>
+            </div>
         </div>
         <div class="pf-body">
             <ul class="extra-details">
 
                 <?php foreach ( $rows as $row ) :
-					$tags = array( "<p>", "</p>" );
-					$row_content = str_replace( $tags, "", $row['content'] );
-				?>
+
+                    $url = preg_match_all('/<a href="(.*)">/', $row['content'], $a );
+
+                    if ( $url ) {
+                        $content = $row['content'];
+                    } else {
+                        $tags = array( "<p>", "</p>" );
+                        $row_content = str_replace( $tags, "", $row['content'] );
+                        $content = ucwords( trim(preg_replace( "#([^a-z0-9])#i", " ", strtolower( $row_content ) ) ) );
+                    }
+                    
+                ?>
                     <li>
                         <div class="item-attr"><?php echo $row['title'] ?></div>
-                        <div class="item-property"><?php echo ucwords( trim( preg_replace( "#([^a-z0-9])#i", " ", strtolower( $row_content ) ) ) );?></div>
+                        <div class="item-property"><?php echo $content;?></div>
                     </li>
                 <?php endforeach ?>
 
