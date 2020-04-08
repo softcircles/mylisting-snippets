74f68f00db790e11c4cf49522e441d21ae9bb285
softcircles <softcircles@outlook.com>
2020-04-08 12:58:09 +0500

    Update listing-quick-view.php

--- my-listing-child/partials/listing-quick-view.php
+++ my-listing-child/partials/listing-quick-view.php
@@ -75,64 +75,22 @@ if ( ! $listing->get_data('geolocation_lat') || ! $listing->get_data('geolocatio
 		           	<div class="lf-item-info">
 		           	    <h4><?php echo apply_filters( 'the_title', $listing->get_name(), $listing->get_id() ) ?></h4>
 
-	                    <?php if ( $info_fields = $listing->get_info_fields() ): ?>
-	                        <ul>
-	                            <?php foreach ( $info_fields as $info_field ): ?>
-	                                 <li>
-	                                    <i class="<?php echo esc_attr( $info_field['icon'] ) ?> sm-icon"></i>
-	                                    <?php echo esc_html( $info_field['content'] ) ?>
-	                                </li>
-	                            <?php endforeach ?>
-	                        </ul>
-	                    <?php endif ?>
+			            <?php
+			            /**
+			             * Include info fields template.
+			             *
+			             * @since 1.0
+			             */
+			            require locate_template( 'templates/single-listing/previews/partials/info-fields.php' ) ?>
 		           	</div>
 
-		            <!-- BUTTONS AT TOP LEFT CORNER -->
-		            <?php if ($options['buttons']): ?>
-		                <div class="lf-head">
-
-		                    <?php foreach ($options['buttons'] as $button): ?>
-
-		                        <?php if ( $button['show_field'] == '__listing_rating' ): ?>
-		                        	<?php if ( $listing->get_rating() ): ?>
-		                                <?php mylisting_locate_template( 'partials/star-ratings.php', [
-		                                    'rating' => $listing->get_rating(),
-		                                    'max-rating' => MyListing\Ext\Reviews\Reviews::max_rating( $listing->get_id() ),
-		                                    'class' => 'listing-rating lf-head-btn rating-preview-card',
-		                                ] ) ?>
-		                        	<?php endif ?>
-		                        <?php elseif ( $button['show_field'] == 'work_hours' ): ?>
-		                            <?php if ( $listing->get_field('work_hours') && $listing->schedule->get_status() !== 'not-available' ): ?>
-		                                <?php $open_now = $listing->get_schedule()->get_open_now(); ?>
-		                                <div class="lf-head-btn open-status <?php echo sprintf( 'listing-status-%s', $open_now ? 'open' : 'closed' ) ?>">
-		                                    <span><?php echo $open_now ? __( 'Open', 'my-listing' ) : __( 'Closed', 'my-listing' ) ?></span>
-		                                </div>
-		                            <?php endif ?>
-		                        <?php else:
-		                        	if ( ! $listing->has_field( $button['show_field'] ) ) {
-		                        		continue;
-		                        	}
-
-		                        	$button_val = $listing->get_field( $button['show_field'] );
-		                        	$button_val = apply_filters( 'case27\listing\preview\button\\' . $button['show_field'], $button_val, $button, $listing );
-
-		                        	if ( is_array( $button_val ) ) {
-		                        		$button_val = join( ', ', $button_val );
-		                        	}
-
-		                        	$GLOBALS['c27_active_shortcode_content'] = $button_val;
-		                        	$btn_content = str_replace( '[[field]]', $button_val, do_shortcode( $button['label'] ) );
-		                        	?>
-		                            <?php if (strlen($btn_content)): ?>
-		                                <div class="lf-head-btn <?php echo has_shortcode($button['label'], '27-format') ? 'formatted' : '' ?>">
-		                                    <?php echo str_replace('[[field]]', $button_val, do_shortcode($button['label'])) ?>
-		                                </div>
-		                            <?php endif ?>
-		                        <?php endif ?>
-
-		                    <?php endforeach ?>
-		                </div>
-		            <?php endif ?>
+			        <?php
+			        /**
+			         * Include head buttons template.
+			         *
+			         * @since 1.0
+			         */
+			        require locate_template( 'templates/single-listing/previews/partials/head-buttons.php' ) ?>
 		        </a>
 
 		        <!-- BACKGROUND GALLERY NAVIGATION BUTTONS -->
