2597a59685f4fa0957ad4eed277e2d7738983e1b
softcircles <softcircles@outlook.com>
2020-10-06 13:49:41 +0500

    Create email-field.php

---
+++ my-listing-child/templates/add-listing/form-fields/email-field.php
@@ -0,0 +1,9 @@
+<input
+	type="email"
+	class="input-text"
+	name="<?php echo esc_attr( isset( $field['name'] ) ? $field['name'] : $key ); ?>"
+	id="<?php echo esc_attr( $key ); ?>" <?php if ( ! empty( $field['required'] ) ) echo 'required'; ?>
+	placeholder="<?php echo esc_attr( $field['placeholder'] ); ?>"
+	value="<?php echo isset( $field['value'] ) ? esc_attr( $field['value'] ) : ''; ?>"
+	oninvalid="this.setCustomValidity('Put  here custom message')"
+>
