84f38fd69386e2f93115643dc85e9efc8d6abc09
softcircles <softcircles@outlook.com>
2020-10-17 12:45:12 +0500

    Update image.php

--- partials/single/cover/image.php
+++ partials/single/cover/image.php
@@ -6,17 +6,25 @@
  */
 
 // Use the empty template if listing cover image isn't available.
+if ( ! ( $image = $listing->get_cover_image( 'full' ) ) ) {
 
-if ( ! ( $image = $listing->get_field( 'gallery' )[0] ) ) {
-    return require locate_template( 'partials/single/cover/none.php' );
+	if ( ! $image = $listing->get_field('gallery') ) {
+	    return require locate_template( 'partials/single/cover/none.php' );
+    }
+
+    $image = $image[0];
 }
 
 // Overlay options.
 $overlay_opacity = c27()->get_setting( 'single_listing_cover_overlay_opacity', '0.5' );
 $overlay_color   = c27()->get_setting( 'single_listing_cover_overlay_color', '#242429' );
+if ( $cover_padding = c27()->get_setting( 'single_listing_cover_height', 35 ) ) {
+	$padding = absint( $cover_padding );
+}
 ?>
 
-<section class="featured-section profile-cover parallax-bg profile-cover-image" style="background-image: url('<?php echo esc_url( $image ) ?>');">
+<section class="featured-section profile-cover profile-cover-image hide-until-load"
+	style="background-image: url('<?php echo esc_url( $image ) ?>'); padding-bottom: <?php echo !empty($padding)?$padding:35; ?>%;">
     <div class="overlay"
          style="background-color: <?php echo esc_attr( $overlay_color ); ?>;
                 opacity: <?php echo esc_attr( $overlay_opacity ); ?>;"
