6686b56182cdf99d31773eba19757093b1d7dbae
softcircles <softcircles@outlook.com>
2021-01-08 15:02:49 +0500

    Update send-email.php

--- my-listing-child/templates/single-listing/quick-actions/send-email.php
+++ my-listing-child/templates/single-listing/quick-actions/send-email.php
@@ -9,7 +9,11 @@ if ( ! ( $email = $listing->get_field('email') ) ) {
 	return;
 }
 
-$link = sprintf( 'mailto:%s?cc=info@fertilitysolutions.co.za&subject=Fertility%20Solutions', $email );
+$subject = sprintf( 'Inquiry to your course on TheraHub: %s', $listing->get_name() );
+$body  = sprintf( 'Hi,  I am interested in your course with the following details that are mentioned below. Course name: %s Course date: date Please reach out with an offer. Best,', $listing->get_name() );
+
+$link = sprintf( 'mailto:%s?subject=%s&body=%s', $email, $subject, $body );
+
 ?>
 
 <li id="<?php echo esc_attr( $action['id'] ) ?>" class="<?php echo esc_attr( $action['class'] ) ?>">
