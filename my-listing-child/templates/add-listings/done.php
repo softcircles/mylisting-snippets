62c3f4acb15a81c1c409a5af1d21c2342b60e8c7
softcircles <softcircles@outlook.com>
2024-12-17 15:08:18 +0500

    Update done.php

--- my-listing-child/templates/add-listings/done.php
+++ my-listing-child/templates/add-listings/done.php
@@ -1,26 +1,24 @@
 <div class="container c27-listing-submitted-notice">
 	<div class="row">
 		<div class="col-md-10 col-md-push-1">
+
 			<div class="element submit-l-message">
 				<div class="pf-head">
 					<div class="title-style-1">
 						<h5>
 							<i class="material-icons">check_circle_outline</i>
 							<?php
-
-							if ( $listing && $listing->author ) {
-						        $listing->author->set_role( 'customer' );
-						    }
-						    
 							switch ( $listing->get_status() ) :
 								case 'publish' :
 									printf( __( 'Listing listed successfully. To view your listing <a href="%s">click here</a>.', 'my-listing' ), $listing->get_link() );
 								break;
 								case 'pending' :
-									printf( __( 'Listing submitted successfully. Your listing will be visible once approved.', 'my-listing' ) );
+									echo __( 'Listing submitted successfully. Your listing will be visible once approved.', 'my-listing' );
+								break;
+								case 'draft' :
+									printf( __( 'Listing saved successfully. To view your listing <a href="%s">click here</a>.', 'my-listing' ), $listing->get_link() );
 								break;
 								default :
-									// do_action( 'job_manager_job_submitted_content_' . str_replace( '-', '_', sanitize_title( $job->post_status ) ), $job );
 								break;
 							endswitch;
 							?>
@@ -34,10 +32,6 @@
 
 <script type="text/javascript">
 	// prevent form resubmission
-	//window.setTimeout(function() {
-	    window.location.href = "https://yourwebsite.com/thank-you/";
-	//}, 5000);
-
 	if ( window.history.replaceState ) {
 		window.history.replaceState( null, null, window.location.href );
 	}
